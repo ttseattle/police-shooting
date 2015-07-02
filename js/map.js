0d26e2de12244c45cd98dd788c26bd30b7d8b9e8
Tisha Trongtham <tishat@attu2.cs.washington.edu>
2015-07-01 21:52:58 -0700

    First try

--- js/map.js
+++ js/map.js
@@ -1,32 +1,39 @@
 // Function to draw your map
 var drawMap() = function() {
-
-  // Create map and set viewd
- 
-
-  // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+  	// Create map and set view
+  	var map = L.map('container');
+  	map.setView([47.6097,-122.3331], 12);
+  	// Create an tile layer variable using the appropriate url
+  	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+  	// Add the layer to your map
+  	layer.addTo(map);
+  	// Execute your function to get data
+  	getData();
 }
 
 // Function for getting data
 var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+	var data;
+  	// Execute an AJAX request to get the data in data/response.js
+  	$.ajax({
+  		url: 'data/response.json',
+  		type: "get", 
+  		// When your request is successful, call your customBuild function
+  		success: customBuild(dat),
+  		dataType: "json"
+  	});
 }
 
 // Do something creative with the data here!  
-var customBuild = function() {
-
-  
+var customBuild = function(dat) {
+	data = dat;
+	data.map(function(d) {
+		var marker = new L.circleMarker([d.latitude, d.longitude], {
+			color: red;
+			radius: 4;
+		});
+		marker.addTo(map);
+	});
 }
 
 
