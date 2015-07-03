6c5dea4b147b0ed00f3fecaf0b10ab4afb9d7c1b
Tisha Trongtham <ttseattle@yahoo.com>
2015-07-03 11:37:12 -0700

    why isn't this working

--- js/map.js
+++ js/map.js
@@ -47,11 +47,44 @@ var getData = function(map) {
   });
 }
 
-// Do something creative with the data here!  
 var customBuild = function(data, map) {
+  var min = 2015;
+  var max = 0;
+  data.map(function(d){
+    
+    if (d["Date Searched"]) {
+      var year = parseInt(d["Date Searched"].match("\\d{4}")[0]);
+      if (year > max) max = year;
+      if (year < min) min = year;
+    }
+    var circle = new L.circle([d.lat, d.lng], 200, {color:'blue', opacity:.5}).addTo(map);
+    var info = document.createElement("p");
+    info.innerHTML = "Agency Name: " + d["Agency Name"] + "<br />"
+    + "Armed or Unarmed?: " + d["Armed or Unarmed?:"] + "<br />"
+    + "Date Searched: " + d["Date Searched"] + "<br />"
+    + "Hispanic or Latino Origin: " + d["Hispanic or Latino Origin"] + "<br />"
+    + "Hit or Killed?: " + d["Hit or Killed?"] + "<br />"
+    + "Race: " + d["Race"] + "<br />"
+    + "Source Link: " + d["Source Link"] + "<br />"
+    + "City: " + d["City"] + "<br />"
+    + "State: " + d["State"] + "<br />"
+    + "Summary: " + d["Summary"] + "<br />"
+    + "Timestamp: " + d["Timestamp"] + "<br />"
+    + "Victim Name: " + d["Victim Name"] + "<br />"
+    + "Victim's Age: " + d["Victim's Age"] + "<br />"
+    + "Victim's Gender: " + d["Victim's Gender"] + "<br />"
+    + "Weapon: " + d["Weapon"];
+    $("#info").replaceWith(info);
+  });
+  console.log("min " + min);
+  console.log("max " + max);
+}
+
+// Do something creative with the data here!  
+/*var customBuild = function(data, map) {
   data.map(function(d) {
     var marker = new L.marker([d.lat, d.lng]);
-  });
+  });*/
 /*
   var year = d.year;
   var fifteen = L.layerGroup(year == 2015);
@@ -63,5 +96,5 @@ var customBuild = function(data, map) {
   marker.addTo(map);
   var text = d.offense_type;
   marker.bindPopup(text); */
-}
+//}
 
