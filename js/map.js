3b58cfb9c5436cbe2bba58510da6d9ddc3c6836d
Tisha Trongtham <tishat@attu4.cs.washington.edu>
2015-07-03 10:54:57 -0700

    experimenting with layers

--- js/map.js
+++ js/map.js
@@ -4,14 +4,24 @@ var drawMap = function() {
   var map = L.map('container');
   map.setView([47.6097,-122.3331], 10);
   // Create an tile layer variable using the appropriate url
-  var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  var lightLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'tishat.mk86k05g',
+    id: 'tishat.mk8ll0eh',
+    accessToken: 'pk.eyJ1IjoidGlzaGF0IiwiYSI6ImYyNTgxNTRlOTExMGQ1Y2QxNjYxMjAwZjFhYWFjZTM5In0.bmOXsyE0G3gZsQrlPUesRA'
+  });
+  var darkLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'tishat.mk8m662m',
     accessToken: 'pk.eyJ1IjoidGlzaGF0IiwiYSI6ImYyNTgxNTRlOTExMGQ1Y2QxNjYxMjAwZjFhYWFjZTM5In0.bmOXsyE0G3gZsQrlPUesRA'
   });
   // Add the layer to your map
-  layer.addTo(map);
+  var timeMaps = {
+    "Daytime": lightLayer,
+    "Nighttime": darkLayer
+  };
+  L.control.layers(timeMaps).addTo(map);
   // Execute your function to get data
   getData(map);
 }
@@ -41,13 +51,6 @@ var customBuild = function(d, map) {
   var year = d.year;
   var fifteen = L.layerGroup(year == 2015);
   var fourteen = L.layerGroup(year == 2014);
-
-  var yearMaps = {
-    "2015": fifteen,
-    "2014": fourteen
-  };
-
-  L.control.layers(yearMaps).addTo(map);
 
   var color = d.offense_type.indexOf('THEFT') != -1 ? 'red' : 'blue';
   var marker = new L.circleMarker([d.latitude, d.longitude], {color: color, radius: 4});  
