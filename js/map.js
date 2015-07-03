1fe803f6c7d322a9516ef91fa6c531b36769c0fb
Tisha Trongtham <tishat@attu4.cs.washington.edu>
2015-07-03 10:58:42 -0700

    more style changes

--- js/map.js
+++ js/map.js
@@ -4,6 +4,12 @@ var drawMap = function() {
   var map = L.map('container');
   map.setView([47.6097,-122.3331], 10);
   // Create an tile layer variable using the appropriate url
+  var beginningLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'tishat.mk86k05g',
+    accessToken: 'pk.eyJ1IjoidGlzaGF0IiwiYSI6ImYyNTgxNTRlOTExMGQ1Y2QxNjYxMjAwZjFhYWFjZTM5In0.bmOXsyE0G3gZsQrlPUesRA'
+  });
   var lightLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
@@ -17,6 +23,7 @@ var drawMap = function() {
     accessToken: 'pk.eyJ1IjoidGlzaGF0IiwiYSI6ImYyNTgxNTRlOTExMGQ1Y2QxNjYxMjAwZjFhYWFjZTM5In0.bmOXsyE0G3gZsQrlPUesRA'
   });
   // Add the layer to your map
+  beginningLayer.addTo(map);
   var timeMaps = {
     "Daytime": lightLayer,
     "Nighttime": darkLayer
