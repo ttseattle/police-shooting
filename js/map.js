53bd5bcf29154bc6dcbf79318e08196e9eaf77f3
Tisha Trongtham <ttseattle@yahoo.com>
2015-07-03 11:39:12 -0700

    work pls

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 var drawMap = function() {
   // Create map and set view
   var map = L.map('container');
-  map.setView([47.6097,-122.3331], 10);
+  map.setView([47.6097,-122.3331], 5);
   // Create an tile layer variable using the appropriate url
   var beginningLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -48,36 +48,9 @@ var getData = function(map) {
 }
 
 var customBuild = function(data, map) {
-  var min = 2015;
-  var max = 0;
   data.map(function(d){
-    
-    if (d["Date Searched"]) {
-      var year = parseInt(d["Date Searched"].match("\\d{4}")[0]);
-      if (year > max) max = year;
-      if (year < min) min = year;
-    }
     var circle = new L.circle([d.lat, d.lng], 200, {color:'blue', opacity:.5}).addTo(map);
-    var info = document.createElement("p");
-    info.innerHTML = "Agency Name: " + d["Agency Name"] + "<br />"
-    + "Armed or Unarmed?: " + d["Armed or Unarmed?:"] + "<br />"
-    + "Date Searched: " + d["Date Searched"] + "<br />"
-    + "Hispanic or Latino Origin: " + d["Hispanic or Latino Origin"] + "<br />"
-    + "Hit or Killed?: " + d["Hit or Killed?"] + "<br />"
-    + "Race: " + d["Race"] + "<br />"
-    + "Source Link: " + d["Source Link"] + "<br />"
-    + "City: " + d["City"] + "<br />"
-    + "State: " + d["State"] + "<br />"
-    + "Summary: " + d["Summary"] + "<br />"
-    + "Timestamp: " + d["Timestamp"] + "<br />"
-    + "Victim Name: " + d["Victim Name"] + "<br />"
-    + "Victim's Age: " + d["Victim's Age"] + "<br />"
-    + "Victim's Gender: " + d["Victim's Gender"] + "<br />"
-    + "Weapon: " + d["Weapon"];
-    $("#info").replaceWith(info);
   });
-  console.log("min " + min);
-  console.log("max " + max);
 }
 
 // Do something creative with the data here!  
